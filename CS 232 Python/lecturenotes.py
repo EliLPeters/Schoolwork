37f3b16e772ffc8396162ce34514f18285cde74c
Eli Peters <elp5@humboldt.edu>
2018-02-12 13:57:54 -0800

    more lecturenotes.py and week5lab.py created

--- CS 232 Python/lecturenotes.py
+++ CS 232 Python/lecturenotes.py
@@ -10,3 +10,16 @@
 
 def add3(a_number):
     return (a_number + 3)
+
+x = 0
+while True:
+    x+=1
+    if x > 10:
+        break
+
+y = 0
+while y < 20:
+    y += 1
+    if (y % 10 == 0):
+        continue
+    print(y)
