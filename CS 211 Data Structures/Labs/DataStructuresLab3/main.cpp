d9e1d58737d6344c732627fb8ab6496c3421e90f
Eli Peters <elp5@humboldt.edu>
2018-02-12 08:32:00 -0800

    Finished lab 3

--- CS 211 Data Structures/Labs/DataStructuresLab3/main.cpp
+++ CS 211 Data Structures/Labs/DataStructuresLab3/main.cpp
@@ -10,21 +10,173 @@ using namespace std;
 
 double evaluatePostfixExpression(string postfix_string)
 {
-	double result = -1;
+	//string result = "";
 	stack<double> postfix_stack{};
 	vector<string> pieces = StringSplitter::split(postfix_string, " ");
 	
 	for (auto piece : pieces)
 	{
-		
+		if (piece != "+" && piece != "-" && piece != "*" && piece != "/")
+		{
+			postfix_stack.push(stod(piece));
+		}
+		else if (piece == "+")
+		{
+			double val_2 = 0;
+			double val_1 = 0;
+			if (!postfix_stack.empty())
+			{
+				val_2 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+			}
+			if (!postfix_stack.empty())
+			{
+				val_1 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax.";
+				return 0;
+			}
+			postfix_stack.push(val_1 + val_2);
+		}
+		else if (piece == "-")
+		{
+			double val_2 = 0;
+			double val_1 = 0;
+			if (!postfix_stack.empty())
+			{
+				val_2 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+			}
+			if (!postfix_stack.empty())
+			{
+				val_1 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+				return 0;
+			}
+			postfix_stack.push(val_1 - val_2);
+		}
+		else if (piece == "*")
+		{
+			double val_2 = 0;
+			double val_1 = 0;
+			if (!postfix_stack.empty())
+			{
+				val_2 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+			}
+			if (!postfix_stack.empty())
+			{
+				val_1 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+				return 0;
+			}
+			postfix_stack.push(val_1 * val_2);
+		}
+		else if (piece == "/")
+		{
+			double val_2 = 0;
+			double val_1 = 0;
+			if (!postfix_stack.empty())
+			{
+				val_2 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+			}
+			if (!postfix_stack.empty())
+			{
+				val_1 = postfix_stack.top();
+				postfix_stack.pop();
+			}
+			else
+			{
+				cout << "An error occured,  Invalid Postfix syntax." << endl;
+				return 0;
+			}
+			postfix_stack.push(val_1 / val_2);
+		}
 	}
 
-	while (!postfix_stack.empty())
+	/*while (!postfix_stack.empty())
 	{
-		
-	}
+		if (postfix_stack.top() == "+")
+		{
+			postfix_stack.pop();
 
-	return result;
+			double val_1 = stod(postfix_stack.top());
+			postfix_stack.pop();
+			double val_2 = stod(postfix_stack.top());
+			postfix_stack.pop();
+
+			postfix_stack.push(to_string(val_2 + val_1));
+		}
+		if (postfix_stack.top() == "-")
+		{
+			postfix_stack.pop();
+
+			double val_1 = stod(postfix_stack.top());
+			postfix_stack.pop();
+			double val_2 = stod(postfix_stack.top());
+			postfix_stack.pop();
+
+			postfix_stack.push(to_string(val_2 - val_1));
+		}
+		if (postfix_stack.top() == "*")
+		{
+			postfix_stack.pop();
+
+			double val_1 = stod(postfix_stack.top());
+			postfix_stack.pop();
+			double val_2 = stod(postfix_stack.top());
+			postfix_stack.pop();
+
+			postfix_stack.push(to_string(val_2 * val_1));
+		}
+		if (postfix_stack.top() == "/")
+		{
+			postfix_stack.pop();
+
+			double val_1 = stod(postfix_stack.top());
+			postfix_stack.pop();
+			double val_2 = stod(postfix_stack.top());
+			postfix_stack.pop();
+
+			postfix_stack.push(to_string(val_2 / val_1));
+		}
+		else
+		{
+			result = stod(postfix_stack.top());
+			postfix_stack.pop();
+		}
+	}
+	*/
+
+	return postfix_stack.top();
 }
 
 int main(void)
