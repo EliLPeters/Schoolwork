f32d2a13ebb43655ee240d79e4cc6c0c95e78480
Eli <elp5@humboldt.edu>
2018-02-26 12:48:18 -0800

    Lab 6 progress (mostly done)

--- CS 211 Data Structures/Labs/Lab 6/ADTs/main.cpp
+++ CS 211 Data Structures/Labs/Lab 6/ADTs/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <queue>
+
 #include "sorting\Sorting.h"
 #include "ADTs\BinarySearchTree.h"
 #include "ADTs\TreeTraversal.h"
@@ -7,24 +9,120 @@
 
 using namespace std;
 
+int getHeight(BinaryNode<int>* node)
+{
+	if (node == nullptr)
+	{
+		return -1;
+	}
+	else if (node->isLeaf())
+	{
+		return 0;
+	}
+	int left_height = getHeight(node->getLeftChild());
+	int right_height = getHeight(node->getRightChild());
+	if (left_height > right_height)
+	{
+		return 1 + left_height;
+	}
+	else
+	{
+		return 1 + right_height;
+	}
+}
+
 bool isAvl(BinarySearchTree<int> *tree)
 {
-    return false;
+	BinaryNode<int>* root = tree->getRoot();
+	if (root != nullptr)
+	{
+		int left_height = getHeight(root->getLeftChild());
+		int right_height = getHeight(root->getRightChild());
+		int difference = right_height - left_height;
+
+		if (difference >= -1 && difference <= 1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return true;
+	}
 }
 
 BinaryNode<int>* rotateLeft(BinaryNode<int>* original_root)
 {
-    return original_root;
+	BinaryNode<int>* temp = original_root->getRightChild;
+	original_root->setRightChild(temp->getLeftChild());
+	temp->setLeftChild(original_root);
+	return temp;
 }
 
 bool isFull(BinarySearchTree<int> *tree)
 {
-    return false;
+	queue<BinaryNode<int>*> to_visit{};
+
+	to_visit.push(tree->getRoot());
+
+	while (to_visit.empty() == false)
+	{
+		BinaryNode<int>* curr = to_visit.front();
+		to_visit.pop();
+
+		if (curr != nullptr)
+		{
+			if ((curr->getRightChild() == nullptr && curr->getLeftChild() != nullptr) || (curr->getRightChild() != nullptr && curr->getLeftChild() == nullptr))
+			{
+				return false;
+			}
+			else if (curr->getRightChild() != nullptr && curr->getLeftChild() != nullptr)
+			{
+				to_visit.push(curr->getLeftChild());
+				to_visit.push(curr->getRightChild());
+			}
+		}
+	}
+
+    return true;
 }
 
 bool isComplete(BinarySearchTree<int>* tree)
 {
-    return false;
+	queue<BinaryNode<int>*> to_visit{};
+
+	bool has_seen_null = false;
+
+	to_visit.push(tree->getRoot());
+
+	// Begin BFS
+	while (to_visit.empty() == false)
+	{
+		BinaryNode<int>* front = to_visit.front();
+		to_visit.pop();
+
+		if (front == nullptr)
+		{
+			has_seen_null = true;
+		}
+		else
+		{
+			// found a gap, therefore not complete
+			if (has_seen_null == true)
+			{
+				return false;
+			}
+			// otherwise push on children
+			to_visit.push(front->getLeftChild());
+			to_visit.push(front->getRightChild());
+		}
+	}
+
+	return true;
 }
 
 int main(void)
@@ -76,7 +174,7 @@ int main(void)
     traversal.traverse(rotated);
     
     //rotate again just for fun
-    rotated = rotateLeft(tree.getRoot());
+    rotated = rotateLeft(rotated);
     traversal.traverse(rotated);
     return 0;
 }
