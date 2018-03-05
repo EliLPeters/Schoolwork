7f4c301c604030af69f99678307e7754bbeae266
Eli Peters <elp5@humboldt.edu>
2018-03-05 09:40:06 -0800

    Finally fixed the FUCKING searchHelper function

--- CS 211 Data Structures/Homework/PA3/TrieTextPredict - starter code/TrieTextPredict/Trie.h
+++ CS 211 Data Structures/Homework/PA3/TrieTextPredict - starter code/TrieTextPredict/Trie.h
@@ -1,10 +1,13 @@
 #ifndef TRIE_H
 #define TRIE_H
 
-#include "TrieNode.h"
 #include <string>
 #include <cctype>
 #include <vector>
+#include <stack>
+
+#include "TrieNode.h"
+
 using namespace std;
 
 class Trie
@@ -16,11 +19,14 @@ private:
 	{
 		char to_add = remaining[0];
 		
+		// If the first letter doesn't exist yet, create it
 		if (parent->getChild(to_add) == nullptr)
 		{
 			parent->setChild(to_add, new TrieNode{ to_add });
 		}
 
+		// If it was passed a string longer than 1 recur with the remaining letters
+		// Otherwise give it a sentinel as a child.
 		if (remaining.length() > 1)
 		{
 			addWordHelper(parent->getChild(to_add), remaining.substr(1));
@@ -33,7 +39,21 @@ private:
 
 	TrieNode* searchHelper(string word, TrieNode* parent)
 	{
-		return nullptr;
+		if (parent == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			if (word.length() > 1)
+			{
+				return searchHelper(word.substr(1), parent->getChild(word.at(0)));
+			}
+			else
+			{
+				return parent->getChild(word[0]);
+			}
+		}
 	}
 
 	void matchHelper(vector<string>& matches, string& match, TrieNode* node)
@@ -44,7 +64,7 @@ private:
 			return;
 		}
 
-		if (node->getValue() == '$')
+		if (node->isSentinel())
 		{
 			matches.push_back(match);
 		}
@@ -71,12 +91,15 @@ public:
 	//TODO: implement
     void addWord(const string &word)
     {
+		// if _root doesn't have the first letter as a child create it
 		if (_root->getChild(word[0]) == nullptr)
 		{
 			_root->setChild(word[0], new TrieNode{ word[0] });
 		}
-
+		
+		// then run the helper function on that child
 		addWordHelper(_root->getChild(word[0]), word.substr(1));
+		
     }
 
 	//TODO: implement
@@ -84,13 +107,19 @@ public:
     {
         vector<string> matches;
 
-		string thingy = "";
+		string thingy = word;
 
 		TrieNode* start = searchHelper(word, _root);
 		// Navigate through the tree until we hit the final letter, then run the matchHelper function starting there.
 		if (start != nullptr)
 		{
-			matchHelper(matches, thingy, start);
+			cout << "NOT NULL! :D" << endl;
+			cout << start->getValue() << endl;
+			//matchHelper(matches, thingy, start);
+		}
+		else
+		{
+			cout << "null" << endl;
 		}
 
         return matches;
